2625f5ef005f9a6e7d2edd37736bdf54d5cd3ee5
ayush_agr101 <ayush.agarwal161204@gmail.com>
2025-03-07 00:46:57 +0530

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [react()],
-  base: '/CoResearchAI.github.io/', // <-- Add this line
+  plugins: [react(),tailwindcss()  ],
+  base: '/CoResearchAI.github.io/', 
+  // <-- Add this line
 });
